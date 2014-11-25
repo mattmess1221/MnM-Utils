66b24fda075c51d77b5aa581ad339f37371e326b
Matthew Messinger <mattmess1221@gmail.com>
2014-11-24 23:48:00 -0500

    Make the tweak tools actually useful. Fix always returning false because the Tweaks list is empty.

--- src/main/java/mnm/mods/util/TweakTools.java
+++ src/main/java/mnm/mods/util/TweakTools.java
@@ -1,49 +1,35 @@
 package mnm.mods.util;
 
-import java.util.Iterator;
-import java.util.List;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
-import net.minecraft.launchwrapper.ITweaker;
-import net.minecraft.launchwrapper.Launch;
+import com.mumfrey.liteloader.core.LiteLoader;
 
 public class TweakTools {
 
     /**
-     * Returns whether LiteLoader is loaded into LaunchWrapper.
+     * Returns whether LiteLoader is currently loaded.
      *
      * @return True if it's loaded
      */
     public static boolean isLiteLoaderLoaded() {
-        return isTweakLoaded("com.mumfrey.liteloader.launch.LiteLoaderTweaker");
+        boolean loaded = false;
+        try {
+            loaded = LiteLoader.getInstance() != null;
+        } catch (Throwable t) {
+        }
+        return loaded;
     }
 
     /**
-     * Returns whether FML is loaded into LaunchWrapper.
+     * Returns whether FML is currently loaded.
      *
      * @return True if it's loaded
      */
     public static boolean isFMLLoaded() {
-        return isTweakLoaded("net.minecraftforge.fml.common.launcher.FMLTweaker");
-    }
-
-    /**
-     * Gets if a given tweak is loaded into LaunchWrapper.
-     *
-     * @param name The fully qualified class name
-     * @return True if it's loaded
-     */
-    public static boolean isTweakLoaded(String name) {
-        boolean load = false;
-        @SuppressWarnings("unchecked")
-        List<ITweaker> tweakers = (List<ITweaker>) Launch.blackboard.get("Tweaks");
-
-        // Iterate through the tweaks and check the class name
-        Iterator<ITweaker> iter = tweakers.iterator();
-        while (!load && iter.hasNext()) {
-            ITweaker tweak = iter.next();
-            String className = tweak.getClass().getName();
-            load = className.equals(name);
+        boolean loaded = false;
+        if (ForgeUtils.FML_INSTALLED) {
+            loaded = FMLCommonHandler.instance() != null;
         }
-        return load;
+        return loaded;
     }
 }
