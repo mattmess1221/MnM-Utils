9a93fa724bd8c92f618c9efcbdd90e040bd43379
Matthew Messinger <mattmess1221@gmail.com>
2015-03-16 21:03:03 -0400

    Make fml and liteloader mod ids the same. Change version on build.

--- src/main/java/mnm/mods/util/MnmUtils.java
+++ src/main/java/mnm/mods/util/MnmUtils.java
@@ -2,5 +2,5 @@ package mnm.mods.util;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(name = "MnM Mod Utils", modid = "MnmModUtils", version = "0.0.1")
+@Mod(name = "Mnm Utils", modid = "mnmUtils", version = "@VERSION@")
 public class MnmUtils {}
