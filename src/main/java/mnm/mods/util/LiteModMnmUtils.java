f04f22d0ad04a1fc09c53bbdd9ca26ee38448b9a
Matthew Messinger <mattmess1221@gmail.com>
2015-10-01 21:25:26 -0400

    Switch to using LiteLoader and contain a LiteMod file. May not build by itself yet.

---
+++ src/main/java/mnm/mods/util/LiteModMnmUtils.java
@@ -0,0 +1,31 @@
+package mnm.mods.util;
+
+import java.io.File;
+
+import com.mumfrey.liteloader.LiteMod;
+
+public class LiteModMnmUtils implements LiteMod {
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void init(File arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void upgradeSettings(String arg0, File arg1, File arg2) {
+        // TODO Auto-generated method stub
+
+    }
+}
