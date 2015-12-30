1401e9b3c9a5af9894924507c2e5de409ca961cc
Matthew Messinger <mattmess1221@gmail.com>
2015-12-30 03:31:29 -0500

    Overload drawChat with color and shadow options

--- src/main/java/mnm/mods/util/text/FancyFontRenderer.java
+++ src/main/java/mnm/mods/util/text/FancyFontRenderer.java
@@ -16,7 +16,19 @@ public class FancyFontRenderer extends Gui {
         this.fontRenderer = fr;
     }
 
+    public void drawChat(IChatComponent chat, int x, int y) {
+        this.drawChat(chat, x, y, true);
+    }
+
     public void drawChat(IChatComponent chat, int x, int y, boolean shadow) {
+        drawChat(chat, x, y, -1, shadow);
+    }
+
+    public void drawChat(IChatComponent chat, int x, int y, int color) {
+        this.drawChat(chat, x, y, color, true);
+    }
+
+    public void drawChat(IChatComponent chat, int x, int y, int color, boolean shadow) {
 
         int x1 = x;
         List<IChatComponent> list = (chat.getSiblings());
